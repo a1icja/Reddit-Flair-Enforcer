5a65920253b3f064ef141eacd97186e05d638df2
SirPacker <git@cheezewerks.com>
2018-08-16 01:50:54 -0400

    Don't exceed Reddit's ratelimits

--- src/handlers/dbCheck.js
+++ src/handlers/dbCheck.js
@@ -8,7 +8,9 @@ module.exports = (db, snoowrap) => {
     
     if (await post.link_flair_text || await post.link_flair_css_class)
       return db.delete(id);
-
+    
+    sleep(1000);
+    
     await post.reply(
 `**Unfortunately, we've had to remove your post.** 
 ___
@@ -27,10 +29,21 @@ ___
 [**Here are our subreddit rules.**](https://www.reddit.com/r/FortNiteBR/wiki/rules) - If you have any queries about this, you can contact us via [Moderator Mail](https://www.reddit.com/message/compose?to=%2Fr%2FFortNiteBR).`
     ).then(c => c.distinguish({ sticky: true }));
 
+    sleep(1000);
+
     await post.remove();
+    sleep(1000);
     await post.lock();
+    sleep(1000);
 
     await db.delete(id);
     return;
   });
 };
+
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
