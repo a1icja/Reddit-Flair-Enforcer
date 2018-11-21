5d712d86a8604528650c891ebd077610d08b5e35
SirPacker <git@cheezewerks.com>
2018-11-21 12:13:10 -0500

    Add Link Suggestion Removal for r/FortniteBR

--- index.py
+++ index.py
@@ -4,10 +4,7 @@ import time
 import json
 from threading import Thread
 
-with open('./config.json') as json_data_file: # Load config file
-    config = json.load(json_data_file)
-
-removalText = """
+flairRemovalText = """
 **Unfortunately, we've had to remove your post.**
 ___
 
@@ -25,7 +22,24 @@ ___
 [**Here are our subreddit rules.**](https://www.reddit.com/r/{}/wiki/rules) - If you have any queries about this, you can contact us via [Moderator Mail](https://www.reddit.com/message/compose?to=%2Fr%2F{}).
 """
 
-class FortniteOverlord:
+suggestionRemovalText = """
+**Unfortunately, we've had to remove your post.**
+___
+
+
+### Suggestion Link Posts
+
+We do not allow suggestions to be link posts on /r/FortniteBR. We require them to be made as text posts with context to describe your suggestion. Sites such as Gfycat, Streamable or Imgur can be used to upload images or videos in your text post.
+
+
+___
+[**Here are our subreddit rules.**](https://www.reddit.com/r/{}/wiki/rules) - If you have any queries about this, you can contact us via [Moderator Mail](https://www.reddit.com/message/compose?to=%2Fr%2F{})
+"""
+
+with open('./config.json') as json_data_file:  # Load config file
+    config = json.load(json_data_file)
+
+class TiltedTowersBot:
     def __init__ (self):
         
         self.loginTime = time.time()  # Define when the bot was logged in
@@ -81,14 +95,26 @@ class FortniteOverlord:
 
                     if (post.link_flair_text is None or post.link_flair_css_class is None): # Check if the flair doesn't exist
 
-                        # Reply with the removal reason and then distinguish/sticky the comment
-                        (post.reply(removalText.format(data['sub'], data['sub']))).mod.distinguish(how='yes', sticky=True)
+                        # Reply with the flair removal reason and then distinguish/sticky the comment
+                        (post.reply(flairRemovalText.format(data['sub'], data['sub']))).mod.distinguish(how='yes', sticky=True)
 
                         post.mod.remove() # Remove the original post
                         post.mod.lock() # Lock the original post
 
-                        print('Post Removed: {} | {}'.format(post.id, time.time()))  # Log that a post was removed
+                        print('Post Removed (No Flair): {} | {}'.format(post.id, time.time()))  # Log that a post was removed
+
+                    # Check if post is on r/FortniteBR, has suggestion flair, and is a link post
+                    elif (data["sub"] is "fortnitebr" and post.is_self is False and post.link_flair_css_class is "suggestion"):
+
+                        # Reply with the suggestion removal reason and then distinguish/sticky the comment
+                        (post.reply(suggestionRemovalText.format(data['sub'], data['sub']))).mod.distinguish(how='yes', sticky=True)
+
+                        post.mod.remove() # Remove the original post
+                        post.mod.lock() # Lock the original post
+
+                        print('Post Removed (Link Suggestion): {} | {}'.format(post.id, time.time()))  # Log that a post was removed
+
             except PrawcoreException as e:
                 print(f'Exception: {e}')
 
-FortniteOverlord()
+TiltedTowersBot()
