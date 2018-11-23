ca570ed298befbe99298fe1acb679ebe27805903
Snaacky <snaacky@gmail.com>
2018-11-23 08:31:00 -0500

    pep8'd codebase, made string usage constant, added f-strings where possible for cleaner code, fixed bug that caused suggestion removal not to work, removed comments for self-explanatory code, made variables more specific

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import time
 import json
 from threading import Thread
 
-flairRemovalText = """
+no_flair_removal = """
 **Unfortunately, we've had to remove your post.**
 ___
 
@@ -19,10 +19,9 @@ For more information, please read [this post](https://www.reddit.com/r/FortNiteB
 
 
 ___
-[**Here are our subreddit rules.**](https://www.reddit.com/r/{}/wiki/rules) - If you have any queries about this, you can contact us via [Moderator Mail](https://www.reddit.com/message/compose?to=%2Fr%2F{}).
-"""
+[**Here are our subreddit rules.**](https://www.reddit.com/r/{}/wiki/rules) - If you have any queries about this, you can contact us via [Moderator Mail](https://www.reddit.com/message/compose?to=%2Fr%2F{})."""
 
-suggestionRemovalText = """
+suggestion_removal = """
 **Unfortunately, we've had to remove your post.**
 ___
 
@@ -36,88 +35,71 @@ ___
 [**Here are our subreddit rules.**](https://www.reddit.com/r/{}/wiki/rules) - If you have any queries about this, you can contact us via [Moderator Mail](https://www.reddit.com/message/compose?to=%2Fr%2F{})
 """
 
-with open('./config.json') as json_data_file:  # Load config file
-    config = json.load(json_data_file)
+with open('./config.json') as config_file:
+    config = json.load(config_file)
+
 
 class TiltedTowersBot:
-    def __init__ (self):
-        
-        self.loginTime = time.time()  # Define when the bot was logged in
-        
-        self.reddit = self.login() # Run the function to login to reddit
-        
-        print("--- Logged in to Reddit")
+    def __init__(self):
+        self.start_time = time.time()
+        self.reddit = self.login()
+        print('--- logged in to reddit')
 
-        self.postStorage = list()  # Initalize the storage for posts
-        
-        self.threadStorage = dict()
-
-        print("--- Storage Initalized")
+        self.post_storage = list()
+        self.thread_storage = dict()
+        print('--- storage initalized')
 
         for sub in config['reddit']['subreddits']:
-          self.threadStorage[sub] = Thread(target=self.getPosts, args=[sub])
-          self.threadStorage[sub].start()
-        
-        (Thread(target=self.checkFlair)).start()  # Start processing posts on a seperate thread
+            self.thread_storage[sub] = Thread(
+                target=self.get_posts, args=[sub])
+            self.thread_storage[sub].start()
 
-        print(f"--- {len(self.threadStorage)} Threads Started and Stored")
-
-        print("--- Post Processing Started")
+        Thread(target=self.check_flair).start()
+        print(f'--- {len(self.thread_storage)} threads started and stored')
+        print('--- post processing started')
 
     def login(self):
-        return praw.Reddit( # Login to reddit with details
+        return praw.Reddit(
             client_id=config['reddit']['id'],
             client_secret=config['reddit']['secret'],
             user_agent=config['reddit']['agent'],
             username=config['reddit']['username'],
             password=config['reddit']['password']
         )
-    
-    def getPosts(self, sub):   
-        try:     
-            for post in self.reddit.subreddit(sub).stream.submissions(): # Get submissions when posted in specified subreddit
 
-                if post.created_utc - self.loginTime > 0: # Make sure the post is from after the bot started (Helps prevent double moderation)
-                    self.postStorage.append({'key': post.id, 'sub': sub, 'time': post.created_utc})  # Add post to storage
+    def get_posts(self, sub):
+        try:
+            for post in self.reddit.subreddit(sub).stream.submissions():
+                if post.created_utc - self.start_time > 0:
+                    self.post_storage.append({'key': post.id, 'sub': sub, 'time': post.created_utc})
         except PrawcoreException as e:
-            print(f'Exception: {e}')
-    
-    def checkFlair(self):
+            print(f'exception: {e}')
+
+    def check_flair(self):
         while True:
             try:
-                filtered = filter(lambda x: x['time'] + 30 * 60 - time.time() < 0, self.postStorage) # Filter out all posts that are not older than 30 minutes
+                filtered = filter(lambda x: x['time'] + 30 * 60 - time.time() < 0, self.post_storage)
 
                 for data in filtered:
+                    post = self.reddit.submission(data['key'])
+                    self.post_storage.remove(data)
 
-                    post = self.reddit.submission(data['key']) # Get the submission from reddit
+                    if post.link_flair_text is None or post.link_flair_css_class is None:
+                        post.reply(no_flair_removal.format(data['sub'], data['sub'])).mod.distinguish(how='yes', sticky=True)
+                        post.mod.remove()
+                        post.mod.lock()
+                        print(f'post removed (no flair): {post.id} | {time.time()}')
 
-                    self.postStorage.remove(data) # Remove the post from the bot's cache before doing anything, in order to prevent double moderation
-
-                    if (post.link_flair_text is None or post.link_flair_css_class is None): # Check if the flair doesn't exist
-
-                        # Reply with the flair removal reason and then distinguish/sticky the comment
-                        (post.reply(flairRemovalText.format(data['sub'], data['sub']))).mod.distinguish(how='yes', sticky=True)
-
-                        post.mod.remove() # Remove the original post
-                        post.mod.lock() # Lock the original post
-
-                        print('Post Removed (No Flair): {} | {}'.format(post.id, time.time()))  # Log that a post was removed
-
-                    # Check if post is on r/FortniteBR, has suggestion flair, and is a link post
-                    elif (post.subreddit.display_name is "FortNiteBR" and post.domain is not "self.FortNiteBR" and post.link_flair_css_class is "suggestion"):
-
-                        # Reply with the suggestion removal reason and then distinguish/sticky the comment
-                        (post.reply(suggestionRemovalText.format(data['sub'], data['sub']))).mod.distinguish(how='yes', sticky=True)
-
-                        post.mod.remove() # Remove the original post
-                        post.mod.lock() # Lock the original post
-
-                        print('Post Removed (Link Suggestion): {} | {}'.format(post.id, time.time()))  # Log that a post was removed
-                    
+                    elif post.subreddit.display_name == 'FortNiteBR' and post.is_self == False and post.link_flair_css_class == 'suggestion':
+                        post.reply(suggestion_removal.format(data['sub'], data['sub'])).mod.distinguish(how='yes', sticky=True)
+                        post.mod.remove()
+                        post.mod.lock()
+                        print(f'post removed (link suggestion): {post.id} | {time.time()}')
                     else:
-                        print('All Checks Passed: {} ({}, {}, {})'.format(post.id, post.subreddit.display_name, post.domain, post.link_flair_css_class))
-                    
+                        print(f'all checks passed: {post.id} ({post.subreddit.display_name}, {post.domain}, {post.link_flair_css_class})')
+
             except PrawcoreException as e:
-                print(f'Exception: {e}')
+                print(f'exception: {e}')
+
 
 TiltedTowersBot()
